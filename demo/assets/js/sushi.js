5102393df7ca45f073730f360a90cd1902e742f9
Duncan Beattie <me@duncanbeattie.com>
2012-08-08 14:39:43 +0100

    adding fade transition

--- demo/assets/js/sushi.js
+++ demo/assets/js/sushi.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-	log('loaded')
-	
+$(document).ready(function(){	
 	if($('#sushi')[0]){ sushi($('#sushi'), 'sushi.xml'); $('#sushi .fallback').remove() }
 });
 
@@ -12,12 +10,10 @@ $(document).ready(function(){
  *
  */
 function sushi(obj, path){
-	log('sushi')
 	$.Sushi = obj;
 	$.Sushi.list = []
 	
 	/* parse xml into objects */
-	log(path)
 	$.ajax({type: "GET",url: path,dataType: "xml",success: function(xml) {
 		$(xml).find('folder').each(function(i){
 			var G = new Object();
@@ -34,7 +30,6 @@ function sushi(obj, path){
 	
 	/* listview */
 	$.Sushi.listview = function(){
-		log('listview')
 		for(i=0;i<$.Sushi.list.length;i++){
 			var gal = $.Sushi.list[i]
 			var str = '<h3>'+gal.name+'</h3>'
@@ -46,7 +41,7 @@ function sushi(obj, path){
 	
 	/* galview */
 	$.Sushi.galView = function(n){
-		$('.gal_title').slideToggle('fast', function() {
+		$('.gal_title').slideToggle('normal', function() {
 			$('.gal_grid').remove()
 			var gal = $.Sushi.list[n]
 			var str1 ='<div class="gal_grid"><div class="bar"><h3>'+gal.name+'</h3><span class="close">close</span></div><div class="bigpic">'+gal.imgs[0]+'</div><div class="gal_thms">'			
@@ -61,31 +56,20 @@ function sushi(obj, path){
 	/* swapimg */
 	$.Sushi.swapimg = function(n){
 		var sp = n.split(':')
-		log('n: '+n+' :: n[0]: '+n[0])
-		$('.bigpic img').remove()
-		$('.bigpic').append($.Sushi.list[sp[0]].imgs[sp[1]])
+		$('.bigpic').fadeOut('normal', function() {
+			$('.bigpic img').remove()
+			$('.bigpic').append($.Sushi.list[sp[0]].imgs[sp[1]])
+			$('.bigpic').fadeIn('normal')
+		});
+		
 	}
 	
 	/* close_galview */
 	$.Sushi.close_galview = function(){
 		$('.gal_grid').fadeOut()
-		$('.gal_title').slideToggle('slow', function() {
+		$('.gal_title').slideToggle('normal', function() {
 			$('.gal_grid').remove()
 		});
 	}
 }
 
-
-// Plugins
-
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-log.history = log.history || [];   // store logs to an array for reference
-log.history.push(arguments);
-arguments.callee = arguments.callee.caller; 
-if(this.console) console.log( Array.prototype.slice.call(arguments) );
-};
-
-// make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
